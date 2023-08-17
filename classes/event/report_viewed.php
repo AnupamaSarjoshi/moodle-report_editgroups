b3092d22222e4e1f2020e0d425f8d72d2b4f2fb1
Tim Hunt <T.J.Hunt@open.ac.uk>
2023-08-17 16:15:15 +0100

    M4.2: log events remove all legacy logs methods #711041

--- classes/event/report_viewed.php
+++ classes/event/report_viewed.php
@@ -78,24 +78,4 @@ EOF;
         }
         return new \moodle_url('/report/editgroups/index.php', $params);
     }
-
-    public static function get_legacy_eventname() {
-        return 'report edit groups';
-    }
-
-    /**
-     * Return the legacy event log data.
-     *
-     * @return array|null
-     */
-    protected function get_legacy_logdata() {
-        return array(
-                $this->courseid,
-                "course",
-                "report edit groups",
-                "report/editgroups/index.php?id={$this->courseid}",
-                $this->contextinstanceid
-        );
-    }
-
 }
